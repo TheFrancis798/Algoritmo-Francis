3ffa1a58ef01e068e3c4803b83a843dd1d49945e
TheFrancis798 <153636495+TheFrancis798@users.noreply.github.com>
2023-12-12 11:11:20 -0500

    Add files via upload

---
+++ ALGORITMO FRANCIS.py
@@ -0,0 +1,64 @@
+# Suma dos números enteros
+num1 = 10
+num2 = 20
+suma = num1 + num2
+print("La suma de", num1, "y", num2, "es:", suma)
+
+# Resta dos números de punto flotante
+num3 = 15.5
+num4 = 7.5
+resta = num3 - num4
+print("La resta de", num3, "y", num4, "es:", resta)
+
+# Multiplica dos números enteros
+num5 = 12
+num6 = 15
+mult = num5 * num6
+print("La multiplicación de", num5, "y", num6, "es:", mult)
+
+# Divide dos números de punto flotante
+num7 = 12.3
+num8 = 3.4
+division = num7 / num8
+print("La división de", num7, "y", num8, "es:", division)
+# Nombre completo
+nombre_completo = "Francis Vasquez"
+
+# Longitud del nombre completo
+longitud_nombre = len(nombre_completo)
+
+# Imprime en pantalla
+print("El nombre completo es:", nombre_completo)
+print("La longitud del nombre completo es:", longitud_nombre)
+# Conversión de tipos de datos
+num_entero = 10
+num_flotante = 20.5
+
+# Conversión entero a flotante
+num_flotante_entero = float(num_entero)
+print("La conversión de", num_entero, "a flotante es:", num_flotante_entero)
+
+# Conversión flotante a entero
+num_entero_flotante = int(num_flotante)
+print("La conversión de", num_flotante, "a entero es:", num_entero_flotante)
+# Comentarios y documentación en el código
+
+# Operaciones Básicas y Tipos de Datos
+num1 = 10
+num2 = 20
+suma = num1 + num2
+print("La suma de", num1, "y", num2, "es:", suma)
+
+# Manipulación de Cadenas de Texto
+nombre_completo = "Juan Pérez"
+longitud_nombre = len(nombre_completo)
+print("El nombre completo es:", nombre_completo)
+print("La longitud del nombre completo es:", longitud_nombre)
+
+# Conversión de Tipos de Datos
+num_entero = 10
+num_flotante = 20.5
+num_flotante_entero = float(num_entero)
+print("La conversión de", num_entero, "a flotante es:", num_flotante_entero)
+num_entero_flotante = int(num_flotante)
+print("La conversión de", num_flotante, "a entero es:", num_entero_flotante)
